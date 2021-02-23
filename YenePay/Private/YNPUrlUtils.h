699bf38d58172e3f919f461f4458db4e71c64dba
Akhmed <akhmedshash@gmail.com>
2021-02-24 00:26:54 +0300

    Added private header files

---
+++ YenePay/Private/YNPUrlUtils.h
@@ -0,0 +1,27 @@
+//
+//  YNPUrlUtils.h
+//  YenePaySDK
+//
+//  Created by Ahmed Mohammed Abdurahman on 1/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "YNPPaymentOrder.h"
+#import "YNPPaymentResponse.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YNPUrlUtils : NSObject
+
+/// <#Description#>
+/// @param paymentOrder <#paymentOrder description#>
+/// @param endpoint The endpoint address including the url scheme (eg. "https://www.yenepay.com/checkout/Home/Process")
++ (NSURL *__nullable)checkoutUrlForPaymentOrder:(YNPPaymentOrder *)paymentOrder
+                                       endpoint:(NSString *)endpoint;
+
++ (YNPPaymentResponse *__nullable)parsePaymentResponseUrl:(NSURL *)url;
+
+@end
+
+NS_ASSUME_NONNULL_END
